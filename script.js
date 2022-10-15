00d329e779aca23ee0e8ba8f2eba8f4cda1a8085
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-10-15 17:12:02 +0200

    Add animation lottie

---
+++ script.js
@@ -0,0 +1,11 @@
+LottieInteractivity.create({
+    mode:"scroll",
+    player:'#magic-hand',
+    actions: [
+        {
+            visibility:[0.5,1],
+            type: "seek",
+            frames: [0, 24],
+        },
+    ]
+});
