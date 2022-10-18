64b7ba8d52f9795255fbf9e67aa78eac5799ac87
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-10-18 17:35:57 +0200

    Gif effect OK

--- script.js
+++ script.js
@@ -29,6 +29,61 @@ gsap.ticker.add(() => {
   ySet(pos.y);
 });
 
+// Hover Text image reveal
+let imgHoverLink = gsap.utils.toArray(".img-hover-effect-link");
+let imgWrap = document.querySelector('.img-wrapper');
+let imgItem = document.querySelector('.img-placeholder img');
+
+function moveImg(e){
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+  let tl = gsap.timeline();
+  tl.to(imgWrap,{
+    duration: 1,
+    x: mouseX,
+    y: mouseY,
+    ease: Expo.ease
+  })
+}
+
+function linkHover(e){
+  if (e.type == "mouseenter"){
+    let imgSrc = e.target.dataset.src;
+    let tl = gsap.timeline();
+
+    tl.set(imgItem, {
+      attr: { src : imgSrc} // update imgSrc as in data-src
+    }).to(imgWrap, {
+      autoAlpha: 1,
+      scale: 1,
+    })
+  }
+
+  if (e.type == "mouseleave"){
+    let tl = gsap.timeline();
+    tl.to(imgWrap, {
+      autoAlpha: 0,
+      scale: 0.3,
+    })
+  }
+}
+
+function initAnimation(){
+  imgHoverLink.forEach((link)=> {
+    link.addEventListener('mouseenter', linkHover);
+    link.addEventListener('mouseleave', linkHover);
+    link.addEventListener('mousemove', moveImg);
+  })
+}
+
+function init(){
+  initAnimation();
+}
+
+window.addEventListener("load", () => {
+  init();
+})
+
 /**
  * Animation Lottie
  */
